f051eae8c0550de6233d323883e261eb9b857988
Jorge Rivera <jlriveraya@gmail.com>
2023-02-16 12:54:09 -0500

    intento de agregar el boton edit

--- funciones.js
+++ funciones.js
@@ -20,14 +20,17 @@ const crearContacto = (parentNode, contacto, localData) => {
     let telefonoContacto = document.createElement('p')
     let direccionContacto = document.createElement('p')
     let iconoBorrar = document.createElement('i')
+    let iconoEditar = document.createElement('i')
 
     nombreContacto.innerHTML = contacto.nombre
     telefonoContacto.innerHTML = contacto.telefono
     direccionContacto.innerHTML = contacto.direccion
     iconoBorrar.innerHTML = ''
+    iconoEditar.innerHTML = ''
 
     divContacto.classList.add('tareas')
     iconoBorrar.classList.add('fa-solid', 'fa-trash', 'icono')
+    iconoEditar.classList.add('icono-edit')
 
     iconoBorrar.onclick = () => {
         localData.removeItem(contacto.id)
@@ -38,6 +41,7 @@ const crearContacto = (parentNode, contacto, localData) => {
     divContacto.appendChild(telefonoContacto)
     divContacto.appendChild(direccionContacto)
     divContacto.appendChild(iconoBorrar)
+    divContacto.appendChild(iconoEditar)
 
     parentNode.appendChild(divContacto)
 
